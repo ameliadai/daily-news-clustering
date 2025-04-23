75e04634117dc9450339e0effcd267855bbd7347
SimoneRitt <sgr344@nyu.edu>
2025-04-23 10:49:40 -0400

    updating full data main_pyopt

--- main_pyopt.py
+++ main_pyopt.py
@@ -6,21 +6,18 @@ python main_pyopt.py \
   --start_date 2025-03-01 \
   --end_date 2025-03-31 \
   --input_path ./news/news_2025_03_update.csv \
-  --output_path ./result/pyopt
+  --output_path ./result/pyopt/
 """
 
 import time
 
-from collections import defaultdict
-
 # plotting
 from sklearn.manifold import TSNE
-from sklearn.preprocessing import StandardScaler
-import umap
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-import itertools
+from collections import defaultdict
+import umap
 
 import argparse
 import os
@@ -64,17 +61,18 @@ def vectorize_text(news_text, n_components=100):
     TF-IDF vectorization and dimension reducation of text data.
     """
     print("Preprocessing text data...")
-    lemmatizer = WordNetLemmatizer()
-    stop_words = set(stopwords.words('english'))
-
     ts = time.perf_counter()
     preprocessed_text = []
+
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
     for text in news_text:
         tokens = nltk.word_tokenize(text)
         tokens = [re.sub(r'[^a-zA-Z]', '', token.lower()) for token in tokens]
-        tokens = [token for token in tokens if token not in stop_words and len(token) > 2]
-        tokens = [lemmatizer.lemmatize(token) for token in tokens]
+        tokens = [lemmatizer.lemmatize(token) for token in tokens if token not in stop_words and len(token) > 2]
+
         preprocessed_text.append(' '.join(tokens))
+
     pre_time = time.perf_counter() - ts
     
     print("Vectorizing text data with TF-IDF...")
@@ -87,6 +85,36 @@ def vectorize_text(news_text, n_components=100):
         X = pca.fit_transform(X.toarray())
     
     return X, pre_time
+
+
+@time_func
+def adjust_dbscan_params(X, k=5):
+    """
+    Adjust DBSCAN parameters `eps` and `min_samples` based on the dataset X.
+    """
+    if X.shape[0] < k:
+        return 0.5, 2  # Default fallback for small datasets
+    
+    nbrs = NearestNeighbors(n_neighbors=k).fit(X)
+    distances, _ = nbrs.kneighbors(X)
+    sorted_distances = np.sort(distances[:, k - 1], axis=0)
+    
+    eps = np.percentile(sorted_distances, 90)
+    min_samples = max(2, int(np.log(len(X))))
+    
+    print(f"Adjusted DBSCAN Params → eps: {eps:.4f}, min_samples: {min_samples}")
+    return eps, min_samples
+
+
+@time_func
+def cluster_texts(X, eps=0.5, min_samples=3):
+    """
+    Cluster vectorized news articles using DBSCAN.
+    """
+    print("Clustering text data using DBSCAN...")
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples, metric='cosine')
+    dbscan.fit(X)
+    return dbscan.labels_
 
 
 @time_func
@@ -162,50 +190,44 @@ def plot_cluster(valid_cluster_inds, X, data, topk):
     Plot Cluster Labels in 2D
     Plot Cluster WordClouds
     """
-    # reduce to 2D
-    perplexity = len(X)-1 if len(X) < 31.0 else 30.0
-    x_2d = TSNE(n_components=2, random_state=0, perplexity=perplexity).fit_transform(X)
-
-    # UMAP might be faster for larger datasets
-    # x_2d = umap.UMAP().fit_transform(X)
+    # reduce to 2D (UMAP generally faster for large datasets)
+    x_2d = umap.UMAP().fit_transform(X)
 
     # plot 2D with cluster labels
     colors = plt.cm.rainbow(np.linspace(0, 1, len(valid_cluster_inds)))
-    cluster_fig, ax = plt.subplots(figsize=(8,5))
-    ax.set_title('UMAP Cluster Visualization')
+    cluster_fig, ax = plt.subplots(figsize=(5,3))
+    ax.set_title('TSNE Cluster Visualization')
     i = 0
     for label, indices in valid_cluster_inds.items():
         ax.scatter(x_2d[indices, 0], x_2d[indices, 1], color=colors[i], label=i+1)
         i += 1
-    ax.legend(loc='center right', bbox_to_anchor = (1.0, 0.5))
+    ax.legend(loc='center right', bbox_to_anchor = (1.2, 0.5))
 
     # plot word cloud (article titles and texts)
     top_valid = sorted(valid_cluster_inds, 
                         key=lambda x: len(valid_cluster_inds[x]),
                         reverse=True)[:topk]
-    wc_title_fig, axs_title = plt.subplots(1, topk,
-                                           figsize=(10*topk, 5))
-    wc_text_fig, axs_text = plt.subplots(1, topk,
-                                         figsize=(10*topk, 5))
-    stop = stopwords.words('english')
+    wc_title_fig, axs_title = plt.subplots(topk, 1,
+                                           figsize=(8, 3*topk))
+    wc_text_fig, axs_text = plt.subplots(topk, 1,
+                                         figsize=(8, 3*topk))
     for i in range(topk):
-        cluster_data = data.iloc[valid_cluster_inds[top_valid[i]]]
-        titles = cluster_data.title
-        texts = cluster_data.text
+        titles = data.iloc[valid_cluster_inds[top_valid[i]]]['title']
+        texts = data.iloc[valid_cluster_inds[top_valid[i]]]['text']
 
         wc_title = WordCloud(
             background_color='white',
-            stopwords=stop,
+            stopwords=stopwords.words('english'),
             min_font_size=10,
             width=1000,
-            height=600
+            height=300
         ).generate(' '.join(titles).lower())
         wc_text = WordCloud(
             background_color='white',
-            stopwords=stop,
+            stopwords=stopwords.words('english'),
             min_font_size=10,
             width=1000,
-            height=600
+            height=300
         ).generate(' '.join(texts).lower())
 
         axs_title[i].imshow(wc_title)
@@ -220,7 +242,7 @@ def plot_cluster(valid_cluster_inds, X, data, topk):
 
 
 @time_func
-def process_date(date, data, output_path, topk, eps=0.5, min_samples=3, k=5):
+def process_date(date, data, output_path, topk):
     """
     Process and save selected articles for a specific date.
     """
@@ -233,25 +255,10 @@ def process_date(date, data, output_path, topk, eps=0.5, min_samples=3, k=5):
     vec_time, outputs = vectorize_text(news_text)
     X, pre_time = outputs
 
-    ts = time.perf_counter()
-    print("Clustering text data using DBSCAN...")
-    '''
-    if X.shape[0] < k:
-        return 0.5, 2  # Default fallback for small datasets
-    
-    nbrs = NearestNeighbors(n_neighbors=k).fit(X)
-    distances, _ = nbrs.kneighbors(X)
-    sorted_distances = np.sort(distances[:, k - 1], axis=0)
-    
-    eps = np.percentile(sorted_distances, 90)
-    min_samples = max(2, int(np.log(len(X))))
-    
-    print(f"Adjusted DBSCAN Params → eps: {eps:.4f}, min_samples: {min_samples}")
-    '''
-    dbscan = DBSCAN(eps=eps, min_samples=min_samples, metric='cosine')
-    dbscan.fit(X)
-    labels = dbscan.labels_
-    cluster_time = time.perf_counter() - ts
+    # adjust_time, eps, min_samples = adjust_dbscan_params(X)
+    eps, min_samples = 0.5, 3
+
+    cluster_time, labels = cluster_texts(X, eps=eps, min_samples=min_samples)
     
     # Sanity check - cluster statistics
     n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
@@ -286,36 +293,30 @@ def process_date(date, data, output_path, topk, eps=0.5, min_samples=3, k=5):
 
     # saving plots
     cluster_fig, wc_title_fig, wc_text_fig = figs
-    cluster_fig.savefig(os.path.join(save_path, 'clusters.png'))
+    cluster_ax = cluster_fig.axes[0]
+    lgd = cluster_ax.get_legend()
+    cluster_fig.savefig(os.path.join(save_path, 'clusters.png'), bbox_extra_artists=(lgd,), bbox_inches='tight')
+    plt.close(cluster_fig)
     wc_title_fig.savefig(os.path.join(save_path, 'titles.png'), bbox_inches='tight')
+    plt.close(wc_title_fig)
     wc_text_fig.savefig(os.path.join(save_path, 'texts.png'), bbox_inches='tight')
+    plt.close(wc_text_fig)
 
     print(f"Articles for {date} saved successfully!")
 
     return pre_time, vec_time, cluster_time, select_time, plot_time
-        
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Hot Topic Selection Pipeline")
-    parser.add_argument('--start_date', type=str, required=True, help='Start date in YYYY-MM-DD')
-    parser.add_argument('--end_date', type=str, required=True, help='End date in YYYY-MM-DD')
-    parser.add_argument('--input_path', type=str, required=True, help='Path to the input data')
-    parser.add_argument('--output_path', type=str, required=True, help='Path to save the output data')
-    parser.add_argument('--topk', type=int, help='Number of articles to select', default=3)
 
-    args = parser.parse_args()
-
-    total_ts = time.perf_counter()
-
-    # loading data
+@time_func
+def main(args):
     ts = time.perf_counter()
-    data = pd.read_csv(args.input_path)
-    data.drop_duplicates(subset=['text'], keep='first', inplace=True)
-    data.drop_duplicates(subset=['title'], keep='first', inplace=True)
-    # data = data[data['text'].apply(len) > 800]
-    # data = data[data['text'].apply(len) < 10000]
-    # data = data[data['title'].str.contains('Opinion:') == False]
-    data = data.reset_index(drop = True)
+    df = pd.read_csv(args.input_path)
+    df.drop_duplicates(subset=['text'], keep='first', inplace=True)
+    df.drop_duplicates(subset=['title'], keep='first', inplace=True)
+    # df = df[df['text'].apply(len) > 800]
+    # df = df[df['text'].apply(len) < 10000]
+    # df = df[df['title'].str.contains('Opinion:') == False]
+    data = df.reset_index(drop = True)
     load_time = time.perf_counter() - ts
     
     start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
@@ -323,7 +324,6 @@ if __name__ == '__main__':
     
     date_range = pd.date_range(start=start_date, end=end_date)
     
-    # processing dates
     process_date_times = []
     preprocess_times = []
     vectorize_times = []
@@ -334,8 +334,7 @@ if __name__ == '__main__':
         date_str = current_date.strftime("%Y-%m-%d")
         print(f"\nProcessing date: {date_str}")
 
-        process_time, times = process_date(date_str, data, 
-                                           args.output_path, args.topk) 
+        process_time, times = process_date(date_str, data, args.output_path, args.topk) 
 
         process_date_times.append(process_time)
         preprocess_times.append(times[0])
@@ -343,22 +342,36 @@ if __name__ == '__main__':
         cluster_times.append(times[2])
         select_topk_times.append(times[3])
         plot_times.append(times[4])
-    
-    total_time = time.perf_counter() - total_ts
 
-    process_time = sum(process_date_times)
-    preprocess_time = sum(preprocess_times)
-    vec_time = sum(vectorize_times)
-    cluster_time = sum(cluster_times)
-    select_time = sum(select_topk_times)
-    plot_time = sum(plot_times)
+
+    return (load_time,
+            sum(process_date_times),
+            sum(preprocess_times),
+            sum(vectorize_times),
+            sum(cluster_times),
+            sum(select_topk_times),
+            sum(plot_times))
+        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Hot Topic Selection Pipeline")
+    parser.add_argument('--start_date', type=str, required=True, help='Start date in YYYY-MM-DD')
+    parser.add_argument('--end_date', type=str, required=True, help='End date in YYYY-MM-DD')
+    parser.add_argument('--input_path', type=str, required=True, help='Path to the input data')
+    parser.add_argument('--output_path', type=str, required=True, help='Path to save the output data')
+    parser.add_argument('--topk', type=int, help='Number of articles to select', default=3)
+    
+    args = parser.parse_args()
+    ts = time.perf_counter()
+    total_time, times = main(args)
+    total_time = time.perf_counter() - ts
 
     with open(args.output_path + "/times.txt", "w") as f:
         print(f'Elapsed Time = {total_time} s', file=f)
-        print(f'-- Loading Data = {load_time} s | {load_time/total_time:.3%} total', file=f)
-        print(f'-- Processing Dates = {process_time} s | {process_time/total_time:.3%} total', file=f)
-        print(f'   -- Vectorizing Texts = {vec_time} s | {vec_time/process_time:.3%} Processing Dates', file=f)
-        print(f'      -- Pre-processing Texts = {preprocess_time} s | {preprocess_time/vec_time:.3%} Vectorizing Texts', file=f)
-        print(f'   -- Clustering Texts = {cluster_time} s | {cluster_time/process_time:.3%} Processing Dates', file=f)
-        print(f'   -- Selecting Top Articles = {select_time} s | {select_time/process_time:.3%} Processing Dates', file=f)
-        print(f'      -- Plotting = {plot_time} s | {plot_time/select_time:.3%} Selecting Top Articles\n', file=f)
+        print(f'-- Loading Data = {times[0]} s | {times[0]/total_time:.3%} total', file=f)
+        print(f'-- Processing Dates = {times[1]} s | {times[1]/total_time:.3%} total', file=f)
+        print(f'   -- Vectorizing Texts = {times[3]} s | {times[3]/times[1]:.3%} Processing Dates', file=f)
+        print(f'      -- Pre-processing Texts = {times[2]} s | {times[2]/times[3]:.3%} Vectorizing Texts', file=f)
+        print(f'   -- Clustering Texts = {times[4]} s | {times[4]/times[1]:.3%} Processing Dates', file=f)
+        print(f'   -- Selecting Top Articles = {times[5]} s | {times[5]/times[1]:.3%} Processing Dates', file=f)
+        print(f'      -- Plotting = {times[6]} s | {times[6]/times[5]:.3%} Selecting Top Articles\n', file=f)
